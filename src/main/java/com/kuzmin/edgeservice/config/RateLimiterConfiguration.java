45d4e74484c080f9ca5baaef7ee74e03402fa768
kuzminal <kuzmin.al35@gmail.com>
2021-10-21 15:21:33 +0300

    Добавлена интеграция с Redis для хранения сессий, но сами сессий пока не авторизованные, Security не прикручен еще RateLimiter добавлен

---
+++ src/main/java/com/kuzmin/edgeservice/config/RateLimiterConfiguration.java
@@ -0,0 +1,14 @@
+package com.kuzmin.edgeservice.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimiterConfiguration {
+    @Bean
+    public KeyResolver keyResolver() {
+        return exchange -> Mono.just("ONE_BUCKET");
+    }
+}
